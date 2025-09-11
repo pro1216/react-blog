f1a8f9dd1be009ac4e88f2a997be4e942af7c77c
pro1216 <shotashota73@outlook.com>
2025-09-11 18:16:27 +0900

    検索機能追加

--- backend/db/action.ts
+++ backend/db/action.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from "express";
 import { ObjectId } from "mongodb";
-import { FormData } from "../types/blog";
+import { Blog, FormData } from "../types/blog";
 import { DbService } from "./dbConfig";
 import nodemailer from "nodemailer";
 
@@ -14,21 +14,24 @@ export function userRoutes(dbService: DbService) {
   const router = Router();
 
   router.get("/test", (req: Request, res: Response) => {
-    res.status(200).json({ message: "success" })
+    res.status(200).json({ message: "success" });
   });
   //記事追加
-  router.post("/article", async (req: Request<{}, {}, ArticleRequestBody>, res: Response) => {
-    try {
-      const { title, content } = req.body;
-      const articlesBoddy = { title, content };
-      const db = await dbService.connect();
-      const result = await db.collection("article").insertOne(articlesBoddy)
-      res.status(201).json({ _id: result.insertedId, ...articlesBoddy });
-    } catch (error) {
-      console.error("記事登録エラー", error);
-      throw res.status(500).json({ error: (error as Error).message });
+  router.post(
+    "/article",
+    async (req: Request<{}, {}, ArticleRequestBody>, res: Response) => {
+      try {
+        const { title, content } = req.body;
+        const articlesBoddy = { title, content };
+        const db = await dbService.connect();
+        const result = await db.collection("article").insertOne(articlesBoddy);
+        res.status(201).json({ _id: result.insertedId, ...articlesBoddy });
+      } catch (error) {
+        console.error("記事登録エラー", error);
+        throw res.status(500).json({ error: (error as Error).message });
+      }
     }
-  });
+  );
 
   // 記事全取得
   router.get("/article", async (_req: Request, res: Response) => {
@@ -40,12 +43,50 @@ export function userRoutes(dbService: DbService) {
       res.status(500).json({ error: (error as Error).message });
     }
   });
+  //検索結果取得
+  router.get(
+    "/article/search",
+    async (req: Request, res: Response): Promise<void> => {
+      try {
+        // クエリパラメータを文字列として取得
+        let q = req.query.query as string | undefined;
+
+        const db = await dbService.connect();
+        if (!q || q.trim() === "") {
+          q = "";
+        }
+        const articles = await db
+          .collection("article")
+          .find({
+            $or: [
+              {
+                title: { $regex: q, $options: "i" },
+                content: { $regex: q, $options: "i" },
+              },
+            ],
+          })
+          .toArray();
+        const formatted: Blog[] = articles.map((s) => ({
+          id: Number(s._id),
+          title: s.title,
+          content: s.content,
+          update_time: new Date(),
+          create_time: new Date(),
+        }));
+        res.json(formatted);
+      } catch (error) {
+        res.status(500).json({ error: (error as Error).message });
+      }
+    }
+  );
   //idを取得して取得
   router.get("/article/:id", async (_req: Request, res: Response) => {
     try {
       const id = _req.params.id;
       const db = await dbService.connect();
-      const article = await db.collection("article").findOne({ _id: new ObjectId(id) });
+      const article = await db
+        .collection("article")
+        .findOne({ _id: new ObjectId(id) });
       if (!article) {
         throw res.status(404).json({ message: "記事が見つかりませんでした" });
       }
@@ -56,28 +97,39 @@ export function userRoutes(dbService: DbService) {
   });
 
   //編集した記事を登録
-  router.post("/article/:id", async (req: Request<{}, {}, ArticleRequestBody>, res: Response) => {
-    try {
-      const id = req.params;
-      const { title, content } = req.body;
-      const articlesBoddy = { title, content };
-      const db = await dbService.connect();
+  router.post(
+    "/article/:id",
+    async (req: Request<{}, {}, ArticleRequestBody>, res: Response) => {
+      try {
+        const id = req.params;
+        const { title, content } = req.body;
+        const articlesBoddy = { title, content };
+        const db = await dbService.connect();
 
-      if (!content) {
-        throw res.status(500).json({ message: "記事がNULLです" })
+        if (!content) {
+          throw res.status(500).json({ message: "記事がNULLです" });
+        }
+        const result = await db.collection("article").insertOne(articlesBoddy);
+        res
+          .status(201)
+          .json({ message: "記事を登録しました。", id: result.insertedId });
+      } catch (error) {
+        throw res.status(500).json({ message: "異常なエラーが発生しました" });
       }
-      const result = await db.collection("article").insertOne(articlesBoddy);
-      res.status(201).json({ message: "記事を登録しました。", id: result.insertedId })
-    } catch (error) {
-      throw res.status(500).json({ message: "異常なエラーが発生しました" });
     }
-  })
+  );
 
   //フォームデータ登録
   router.post("/form", async (req: Request, res: Response) => {
     try {
       const { name, email, message }: FormData = req.body as FormData;
-      const formData = { name, email, message, create_time: new Date(), update_time: new Date() };
+      const formData = {
+        name,
+        email,
+        message,
+        create_time: new Date(),
+        update_time: new Date(),
+      };
       const db = await dbService.connect();
       const result = await db.collection("formData").insertOne(formData);
 
@@ -85,6 +137,7 @@ export function userRoutes(dbService: DbService) {
         _id: result.insertedId.toString(), // MongoDB の ObjectId を string に
         ...formData,
       };
+      //メール送信
       await sendEmailNotification(savedData, res);
       res.status(201).json(savedData);
     } catch (error) {
@@ -92,14 +145,18 @@ export function userRoutes(dbService: DbService) {
       throw res.status(500).json({ error: (error as Error).message });
     }
   });
-  async function sendEmailNotification(formData: FormData, res: Response): Promise<void> {
-    // ここにメール送信のロジックを実装
-    // Gmail SMTP を使う例（ローカルでもOK）
+  async function sendEmailNotification(
+    formData: FormData,
+    res: Response
+  ): Promise<void> {
+    //メール送信
+    const user = process.env.EMAIL_USER;
+    const pass = process.env.EMAIL_PASS;
     let transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: "shota19961216@gmail.com",
-        pass: "18Sv41fA4", // Gmailなら「アプリパスワード」を利用
+        user,
+        pass,
       },
     });
 
---
+++ frontend/src/component/search/SearchResult.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import remarkBreaks from "remark-breaks";
+import remarkGfm from "remark-gfm";
+import { Blog } from "../../types/blog";
+import { Common } from "../Common";
+
+export default function SearchResult() {
+  const [articles, setArticles] = useState<Blog[]>([]);
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("query") || "";
+  const urlParams = new URLSearchParams({
+    query: query,
+  });
+  //記事の文字数取得
+  function getContent(content: string): string {
+    if (!content) return "";
+    const strippedContent = content.replace(/[\n\r]/g, ""); // 改行コードを削除
+    const contentLength = strippedContent.length;
+    if (contentLength > 20) {
+      // 20文字を超える場合の処理
+      return strippedContent.slice(0, 20) + "...";
+    }
+    return strippedContent;
+  }
+
+  useEffect(() => {
+    fetchArticles();
+  }, [query]);
+
+  async function fetchArticles() {
+    try {
+      console.log(urlParams.toString());
+      const res = await fetch(
+        `http://localhost:3002/api/article/search?${urlParams.toString()}`
+      );
+      if (!res.ok) {
+        throw new Error(`HTTPエラー:${res.status}`);
+      }
+      const data: Blog[] = await res.json();
+      setArticles(data);
+    } catch (error) {
+      console.error("記事取得失敗", error);
+    }
+  }
+  return (
+    <Common>
+      <main>
+        <h2>アーカイブス</h2>
+        {articles.map((ds) => {
+          let create_time = new Date(ds.create_time);
+          let update_time = new Date(ds.update_time);
+          return (
+            <section>
+              <article>
+                <div id="article-header">{ds.title}</div>
+                <hr />
+                <div id="article-content">
+                  <ReactMarkdown remarkPlugins={[remarkGfm, remarkBreaks]}>
+                    {getContent(ds.content)}
+                  </ReactMarkdown>
+                  {/* <p>作成日 : {ds.create_time.toLocaleDateString()}</p>
+                <p>更新日 : {ds.update_time.toLocaleDateString()}</p> */}
+                </div>
+              </article>
+            </section>
+          );
+        })}
+      </main>
+    </Common>
+  );
+}
